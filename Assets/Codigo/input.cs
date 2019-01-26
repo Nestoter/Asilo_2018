256a3a071ab6373214f62242801f42fe9ba44bf0
mariana_gelos <gelosmariana@gmail.com>
2019-01-26 00:46:54 -0300

    no message

--- Assets/Codigo/input.cs
+++ Assets/Codigo/input.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class input : MonoBehaviour
 {
-    public float speed = 6; 
+    public float speed = 6;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame

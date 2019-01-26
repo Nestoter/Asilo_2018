368475c2ec510bbd293e7cf4b20737c3c6cabc28
mariana_gelos <gelosmariana@gmail.com>
2019-01-26 02:54:35 -0300

    no message

--- Assets/Codigo/input.cs
+++ Assets/Codigo/input.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class input : MonoBehaviour
 {
-    public float speed = 6;
+    public float speed;
+    public float distancia;
+    private enum posicionVertical { abajo, medio, arriba };
+    private posicionVertical a;
+
+    private int unNumero;
+
     // Start is called before the first frame update
     void Start()
     {
+        this.a = posicionVertical.medio;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //Debug.Log("Enum string is "+ a.ToString());
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.rotation = new Quaternion(0, 0, 0, 0);
+            Vector3 pos = transform.position;
+            pos.x += speed * Time.deltaTime;
+            this.transform.position = pos;
+        }
+        
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             this.transform.rotation = new Quaternion(0, 180, 0, 0);
@@ -21,5 +38,25 @@ public class input : MonoBehaviour
             pos.x -= speed * Time.deltaTime;
             this.transform.position = pos;
         }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            switch (a)
+            {
+                case posicionVertical.abajo:
+                    break;
+                case posicionVertical.medio:
+                    Vector3 pos = transform.position;
+                    pos.y += speed * Time.deltaTime;
+                    this.transform.position = pos;
+                    this.a = posicionVertical.arriba;
+                    Debug.Log("Fui a " + a.ToString());
+                    break;
+                case posicionVertical.arriba:
+                    break;
+            }
+
+        }
+
     }
 }
--- Assets/Codigo/movimiento.cs
+++ Assets/Codigo/movimiento.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class movimiento : MonoBehaviour
 {
+    public float speed = 6;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class movimiento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        this.transform.position = pos;
+       
     }
 }

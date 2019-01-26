21718f70003d6c8c8c0d42f638cf8ca839186d2f
mariana_gelos <gelosmariana@gmail.com>
2019-01-26 00:34:45 -0300

    no message

---
+++ Assets/Codigo/input.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class input : MonoBehaviour
+{
+    public float speed = 6; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.rotation = new Quaternion(0, 180, 0, 0);
+            Vector3 pos = transform.position;
+            pos.x -= speed * Time.deltaTime;
+            this.transform.position = pos;
+        }
+    }
+}
